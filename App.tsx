fc22dc54c3627a1fa8b5e142930417df036d0e00
bruna calheiros <calheiros.bruna@gmail.com>
2021-05-10 22:42:26 -0300

    map

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView from 'react-native-maps'
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container}>      
+      <MapView style={styles.map} initialRegion={{
+        latitude:-9.616731,
+        longitude:-35.7079124,
+        latitudeDelta: 0.008,
+        longitudeDelta: 0.008,      
+      }}/>
     </View>
   );
 }
@@ -14,8 +19,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  }
 });
