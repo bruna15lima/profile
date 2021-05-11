4ff919d7ddfafdc682b75f4bffdbd00d0d20d04e
bruna calheiros <calheiros.bruna@gmail.com>
2021-05-11 01:43:02 -0300

    all

--- App.tsx
+++ App.tsx
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import MapView from 'react-native-maps'
-
+import MapView  from 'react-native-maps'
+import { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
+import mapMarker from './assets/marker.png'
 
 export default function App() {
   return (
     <View style={styles.container}>      
-      <MapView style={styles.map} initialRegion={{
+      <MapView 
+      provider={PROVIDER_GOOGLE}
+      style={styles.map} 
+      initialRegion={{
         latitude:-9.616731,
         longitude:-35.7079124,
         latitudeDelta: 0.008,
         longitudeDelta: 0.008,      
-      }}/>
+      }}>
+        <Marker
+          icon={mapMarker}
+          calloutAnchor={{
+            x: 2.7,
+            y: 0.8,
+          }}
+          coordinate={{
+            latitude:-9.616731,
+            longitude:-35.7079124,
+          }}
+        >
+          <Callout tooltip onPress={() =>{alert('oi') }} >
+            <View style={styles.calloutContainer}>
+            <Text style={styles.calloutText}>nome do usuario</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+
+     
     </View>
   );
 }
@@ -24,5 +48,20 @@ const styles = StyleSheet.create({
   map: {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
-  }
+  },
+
+  calloutContainer: {
+     width: 160,
+     height: 46,
+     paddingHorizontal: 16,
+     backgroundColor: 'rgba(255, 255, 255, 0.8)',
+     borderRadius: 16,
+     justifyContent: 'center',
+  },
+
+  calloutText:{
+    color: '#0089a5',
+    fontSize: 14,
+  },
+
 });
